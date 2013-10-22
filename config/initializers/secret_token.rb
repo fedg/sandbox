e240fa524d03dba995b2d7cec7e2d19af8c45521
Federico Galindo <federicogal@gmail.com>
2013-10-22 15:28:22 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sandbox::Application.config.secret_key_base = '4b86bb59724c9d54c8425c973761b23cfc63db246ed481f66ef4725e28781edf0e94c3e6371779477875ef62b2813e739102af531dd7b1bff7aa1eed34efeca6'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Sandbox::Application.routes.draw do
+  get "pages/home"
+  root to: "pages#home"
+end
